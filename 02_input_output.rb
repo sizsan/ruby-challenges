e3a661a67b926d3dc114f7a328305c65161052a9
sizsan <sizsan2001@gmail.com>
2019-04-02 10:49:12 +1000

    Complete Ruby Challenge

--- 02_input_output.rb
+++ 02_input_output.rb
@@ -1,27 +1,27 @@
 # Write a program that accepts someone's name and age, and responds
 # as shown here:
 
-puts "What is your name?"
-    name = gets.capitalize
 
-puts "Hi #{name}, How old are you?"
-age = gets.to_i
 
-puts "Wow! you are #{age} years old. Congratulations!"
-age100 = 100 - age
-puts "#{name}, in #{age100} year you will be 100 years old!"
+
+
+
+
 # Program is run and outputs:
 # "What is your name?"
-
+puts "What is your name?"
+    name = gets.capitalize
 # User inputs: "Tom"
 
 # Program outputs:
 # "Hi Tom! How old are you?"
-
+puts "Hi #{name}, How old are you?"
+age = gets.to_i
 # User inputs: 40
 
 # Program outputs:
 # "Wow! You are 40 years old. Congratulations!"
+puts "Wow! you are #{age} years old. Congratulations!"
 
 # Beast mode challenge:
 # Ater the above, the program calculates how many years until
@@ -30,3 +30,5 @@ puts "#{name}, in #{age100} year you will be 100 years old!"
 # "Wow! You are 40 years old. Congratulations!"
 # The program also outputs:
 # "Tom, in 60 years you will be 100 years old!"
+age100 = 100 - age
+puts "#{name}, in #{age100} year you will be 100 years old!"
